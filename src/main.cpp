ae6d3846b50ec20e99470e14b097b3610deadce9
MarinRobotics <marinrobotics@gmail.com>
2022-03-06 16:59:09 -0800

    (VIS TEST) switched to set power
    NOTE: move_velocity made it not move AT ALL... I have no idea why

--- src/main.cpp
+++ src/main.cpp
@@ -46,13 +46,13 @@ void vision_test () {
 	 //move the arm up and down to keep the signature centered
 	 //check in y dimension
 	 if (nutral_mogii[0].y_middle_coord > 10 && !isDoingStuff_UD){ //NOTE: 10 may be too small a number. Check w/ testing
-		 arm_turntableA.move_velocity(45); //NOTE: check if this is moving in the right direction
-		 arm_turntableB.move_velocity(45); //NOTE: check if this is moving in the right direction
+		 arm_turntableA = 100; //NOTE: check if this is moving in the right direction
+		 arm_turntableB = 100; //NOTE: check if this is moving in the right direction
 		 pros::screen::print(TEXT_LARGE, 1,"arm moving down");
 		 isDoingStuff_UD = true;
 	 } if (nutral_mogii[0].y_middle_coord < -10 && !isDoingStuff_UD) {
-		 arm_turntableA.move_velocity(-45); //NOTE: check if this is moving in the right direction
-		 arm_turntableB.move_velocity(-45); //NOTE: check if this is moving in the right direction
+		 arm_turntableA = -100; //NOTE: check if this is moving in the right direction
+		 arm_turntableB = -100; //NOTE: check if this is moving in the right direction
 		 pros::screen::print(TEXT_LARGE, 1,"arm moving up");
 		 isDoingStuff_UD = true;
 	 } else {
